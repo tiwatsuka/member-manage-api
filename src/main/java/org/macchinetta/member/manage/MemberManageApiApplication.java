fa3fb71ad0a38b05e6cc220276acec5fcfceb054
Takuya Iwatsuka <takuya.iwatsuka@gmail.com>
2016-11-24 23:00:33 +0900

    initial commit

---
+++ src/main/java/org/macchinetta/member/manage/MemberManageApiApplication.java
@@ -0,0 +1,12 @@
+package org.macchinetta.member.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberManageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberManageApiApplication.class, args);
+	}
+}
